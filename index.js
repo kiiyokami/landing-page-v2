e08a2421522e1ff8cef47e9f100372175e4d6bea
Kiiyo <hiimkaze@gmail.com>
2022-10-06 20:34:18 +0800

    finished back end

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-const Router = require('./src/Router.js');
+var express = require('express');
+var app = express();
+var port = 8080;
+var landing = require('./src/routes/landing');
 
-new Router()
-    .load()
-    .catch(console.error);
+app.listen(port, function(err){
+    if (err) console.log("Error in server setup")
+    console.log("Server loaded, listening on Port", port);
+});
+
+    app.use('/', landing);
+
+    
