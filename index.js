f99068aad420904cebcf45b863726860deb6316b
Kiiyo <hiimkaze@gmail.com>
2022-10-07 07:55:43 +0800

    mashiro

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ var port = 8080;
 var landing = require('./src/routes/landing');
 
 app.listen(port, function(err){
-    if (err) console.log("Error in server setup")
+    if (err) console.log("Error in server setup");
     console.log("Server loaded, listening on Port", port);
+
 });
 
     app.use('/', landing);
